1949d6dd883a88cddc6dc527c3cad12f36421ed2
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 21:57:47 -0500

    try cathc

--- actividad3/Test.java
+++ actividad3/Test.java
@@ -6,5 +6,16 @@ public class Test {
 
         colaPrioridad.enqueue("A", 2);
         colaPrioridad.enqueue("B", 5);
+        colaPrioridad.enqueue("C", 1);
+        colaPrioridad.enqueue("D", 4);
+
+        System.out.println("cola ordenada por prioridad: ");
+        System.out.println(colaPrioridad);
+
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
