a07354f3d84c2be7eb954d246b038f4326b83b34
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 22:47:12 -0500

    constructor

--- ejercicio2/QueueArray.java
+++ ejercicio2/QueueArray.java
@@ -4,5 +4,11 @@ public class QueueArray<T> implements Queue<T> {
     private T[] data;
     private int front, rear, size, capacity;
 
-    
+    public QueueArray(int capacity) {
+        this.capacity = capacity;
+        data = (T[]) new Object[capacity];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
 }
