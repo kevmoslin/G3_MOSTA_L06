10057c563f77efa979214ef97eceee9756d47312
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 23:11:00 -0500

    PriorityQueueLinked

--- ejercicio3/PriorityQueueLinked.java
+++ ejercicio3/PriorityQueueLinked.java
@@ -1,5 +1,16 @@
 package ejercicio3;
 
 public class PriorityQueueLinked<T> implements PriorityQueue<T> {
+    private QueueLink<T>[] queues;
+    private int levels;
+
+    public PriorityQueueLinked(int levels) {
+        this.levels = levels;
+        queues = new QueueLink[levels];
+        for (int i = 0; i < levels; i++) {
+            queues[i] = new QueueLink<>();
+        }
+    }
+
     
 }
