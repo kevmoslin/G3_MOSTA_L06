24028fb28d2b6b6fdb21b2022d273af0cb273652
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 20:49:08 -0500

    arregladno

--- actividad2/Test.java
+++ actividad2/Test.java
@@ -4,7 +4,7 @@ import actividad1.ExceptionIsEmpty;
 
 public class Test {
     public static void main(String[] args) {
-        QueueLink<Integer> cola = new QueueLink<>();
+        Queue<Integer> cola = new QueueLink<>();
 
         try {
             cola.enqueue(10);
