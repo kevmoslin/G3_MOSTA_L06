a55545c3d08a422e1d9715ce5063a0fe1b5b32be
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 22:34:53 -0500

    metodo pop

---
+++ ejercicio1/StackLink.java
@@ -0,0 +1,26 @@
+package ejercicio1;
+
+public class StackLink<T> implements Stack<T> {
+    private Node<T> top;
+
+    public StackLink(){
+        this.top = null;
+    }
+
+    public void push(T elemento){
+        Node<T> newNode = new Node<>(elemento);
+        newNode.siguiente = top;
+        top = newNode;
+    }
+
+    public T pop() throws ExceptionIsEmpty{
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("Stack is Empty");
+        }
+        T data = top.data;
+        top = top.siguiente;
+        return data;
+    }
+
+    
+}
