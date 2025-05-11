0c51be8eb787292888cede83a93c23f68d160a60
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 20:42:18 -0500

    quitando elemetno y mostrando como queda la cola y el nuevo frente del elemento

--- actividad2/Test.java
+++ actividad2/Test.java
@@ -9,6 +9,16 @@ public class Test {
         try {
             cola.enqueue(10);
             cola.enqueue(20);
+            cola.enqueue(30);
+
+            System.out.println("cola: " + cola.toString());
+            System.out.println("frente: " + cola.front());
+            System.out.println("ultimo: " + cola.back());
+
+            System.out.println("elemento quitado: " + cola.dequeue());
+
+            System.out.println("nueva cola: " + cola.toString());
+            System.out.println("nuevo frente: " + cola.front());
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
