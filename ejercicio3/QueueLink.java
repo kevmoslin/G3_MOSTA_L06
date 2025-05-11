88bb97abe2c36ec98c1e6a0d3a81edc45590aa47
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 23:01:59 -0500

    imports atributos y constructor

---
+++ ejercicio3/QueueLink.java
@@ -0,0 +1,14 @@
+package ejercicio3;
+
+import ejercicio2.*;
+import ejercicio1.*;
+
+public class QueueLink<T> implements Queue<T> {
+    private Node<T> first, last;
+
+    public QueueLink() {
+        first = last = null;
+    }
+
+    
+}
