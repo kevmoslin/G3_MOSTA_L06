2319523643197f203abe66cbf80043a2e71fd766
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 19:40:34 -0500

    main clase test java

---
+++ actividad1/Test.java
@@ -0,0 +1,10 @@
+package actividad1;
+
+public class Test {
+    public static void main(String[] args){
+        Stack<Integer> pila = new StackArray<>(5);
+        try{
+            pila.push(10);
+        }
+    }
+}
