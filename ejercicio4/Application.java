bd90be953d5d4fce32fa2ebe77b27af1ca1c93d5
kevmoslin <kevinmostcaerolinares@gmail.com>
2025-05-10 23:27:32 -0500

    ciclo for

--- ejercicio4/Application.java
+++ ejercicio4/Application.java
@@ -1,7 +1,12 @@
 package ejercicio4;
 
+import ejercicio1.StackLink;
+
 public class Application {
     public static boolean symbolBalancing(String input){
-        
+        StackLink<Character> stack = new StackLink<>();
+        for (char ch : input.toCharArray()){
+            
+        }
     }
 }
